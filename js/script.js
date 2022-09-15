9e43086c8439616d48241553ff474166c8216277
Akram-Haji-Ali <112483250+Akram-Haji-Ali@users.noreply.github.com>
2022-09-15 18:59:07 +0200

    define a list of pokemon

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
-alert('Hello world');
-let favoriteFood =`Mulukhiyah`;
-document.write (favoriteFood) ;
+// define a list of pokemon 
+
+let pokemonList = [
+    { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+    { name: 'Charmander', height: 0.6, types: ['fire']},
+    { name: 'Squirtle', height: 0.5, types: ['water']},
+    { name: 'Psyduck', height: 0.8 types: ['water']}
+];
