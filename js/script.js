3f6b786cab6dc82e920b8bea3907ba3128e14f33
Akram-Haji-Ali <112483250+Akram-Haji-Ali@users.noreply.github.com>
2022-10-02 00:04:37 +0200

    use the forEach() function&IIFE

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // define a list of pokemon 
-
+let pokemonRepository =(function(){
 let pokemonList = [
     { 
         name: 'Bulbasaur',
@@ -26,11 +26,40 @@ let pokemonList = [
     }
 ];
 
-for (let i = 0; i < pokemonList.length; i++ ) { 
+function getAll() {
+    return pokemonList;
+}
+
+function add(item) {
+    pokemonList.push(item);
+}
+
+return {
+    getAll: getAll,
+    add: add
+}
+})()
+
+
+
+
+
+/*for (let i = 0; i < pokemonList.length; i++ ) { 
     if  (pokemonList[i].height > 0.6) {
       document.write(pokemonList[i].name + ' height: '  + pokemonList[i].height + ' -Wow, that\'s big!' + '<br>' );
     }
      else {
         document.write(pokemonList[i].name + ' height: '  + pokemonList[i].height + '<br>'); // the <br> tag added a line break
     }
-  }
+  }*/
+  
+
+  let heightBig = 0.6;
+
+  pokemonRepository.getAll().forEach( function(pokemon) {
+    if (pokemon.height >= heightBig) {
+        document.write(`<li>${pokemon.name} (height: ${pokemon.height}m) - Wow, that's big!</li>`);
+    } else {
+        document.write(`<li>${pokemon.name} (height: ${pokemon.height}m)</li>`);
+    }
+}) 
